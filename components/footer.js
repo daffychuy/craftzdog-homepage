8697f24c1119b5fc2b3b8c2098ac2fff43d8d400
daffychuy <daffychuy@outlook.com>
2022-07-11 23:12:50 -0400

    Update footer to reflect removed charmander

--- components/footer.js
+++ components/footer.js
@@ -7,8 +7,7 @@ const Footer = () => {
         &copy; {new Date().getFullYear()} Daniel Chu. All Rights Reserved.
       </Box>
       <Box align="center" opacity={0.1} fontSize="sm">
-        Credits: <a href="https://github.com/craftzdog/craftzdog-homepage">Theme</a> | {' '}
-        <a href="https://sketchfab.com/3d-models/charmander-voxel-pixel-f371dd6b2c8f4ba78455f21e8153c358">Charmander Voxel</a>
+        Credits: <a href="https://github.com/craftzdog/craftzdog-homepage">Theme</a>
       </Box>
     </div>
   )
